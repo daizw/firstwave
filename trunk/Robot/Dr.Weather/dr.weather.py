5ef841d8df1a54bef8bf377427b1ec70e4350a2c
shinysky1986 <shinysky1986@9a49b822-6227-11de-b259-67f869b4f23a>
2009-06-30 06:33:22 +0000

    included both C and F in temperature report

--- trunk/Robot/Dr.Weather/dr.weather.py
+++ trunk/Robot/Dr.Weather/dr.weather.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
+__author__ = 'shinysky'
+__license__ = "Apache License"
+__version__ = "2.0"
+
+'''Dr. Weather, a Google Wave robot.
+Gives the weather of a city.
+
+Usage: @city,country
+E.g.: @beijing,china
+'''
+
 import logging
 import re
 
@@ -28,35 +39,18 @@ def OnParticipantsChanged(properties, context):
             break
 
 def OnBlipSubmit(properties, context):
-    """Invoked when new blip submitted.
-    hl = zh-cn: chinese
-         zh-tw: tranditional chinese
-         en: english
-         de:
-         fr:
-    """
-    blip = context.GetBlipById(properties['blipId'])
-    text = blip.GetDocument().GetText()
-    queries = re.findall(r'(?i)@([^,]+(,(\s)?.*)?)', text)
-    #Iterate through search strings
-    for q in queries:
-        city = q[0].replace(' ', '%20')
-        weather_data = pywapi.get_weather_from_google(city)
-        newBlip = blip.GetDocument().AppendInlineBlip()
-        newBlip.GetDocument().SetText(gooleWeatherConverter(weather_data))
-
-def OnBlipSubmitRTF(properties, context):
     """Invoked when new blip submitted. append rich formatted text to blip"""
     blip = context.GetBlipById(properties['blipId'])
     text = blip.GetDocument().GetText()
-    queries = re.findall(r'(?i)@([^,]+(,(\s)?.*)?)', text)
-    newBlip = blip.GetDocument().AppendInlineBlip()
-    doc = newBlip.GetDocument()
+    queries = re.findall(r'(?i)@([^,@]+(,(\s)?[^,@]*)?)', text)
+    if queries:
+        newBlip = blip.GetDocument().AppendInlineBlip()
+        doc = newBlip.GetDocument()
     #Iterate through search strings
     for q in queries:
-        city = q[0].replace(' ', '%20')
+        city = q[0].strip().replace(' ', '%20')
         weather_data = pywapi.get_weather_from_google(city)
-        gooleWeatherConverterRTF(weather_data, doc)
+        gooleWeatherConverter(weather_data, doc)
 
 def Notify(context, message):
     root_wavelet = context.GetRootWavelet()
@@ -71,41 +65,45 @@ def Fahrenheit2Celsius(F):
     except:
         return '(N/A)'
 
-def gooleWeatherConverter(weatherData):
-    '''convert data to html/txt'''
-    text = ''
-    text += 'City: %s\n' % weatherData['forecast_information']['city']
-    text += 'Current Condition: %s\n' % weatherData['current_conditions']['condition']
-    text += '%s\n' % weatherData['current_conditions']['humidity']
-    text += 'Temperature: %s C\n' % weatherData['current_conditions']['temp_c']
-    text += '%s\n' % weatherData['current_conditions']['wind_condition']
-    text += '\nForecasts:\n'
-    for day in weatherData['forecasts']:
-        text += '%s: %s, %sC ~ %sC\n'%(day['day_of_week'],
-                day['condition'],
-                Fahrenheit2Celsius(day['low']),
-                Fahrenheit2Celsius(day['high']))
-    return text
+def Celsius2Fahrenheit(C):
+    '''convert C to F'''
+    try:
+        c = int(C)
+        f = int(round(c*9.0/5.0+32))
+        return str(f)
+    except:
+        return '(N/A)'
+
+def TempConverter(t, u):
+    '''temp temparature converter
+    
+    if we support multi languages in the future,
+    this func is not needed any more.
+    '''
+    if u.upper() == 'US':
+        return '%sC(%sF)'%(Fahrenheit2Celsius(t),t)
+    else:# u == 'SI':
+        return '%sC(%sF)'%(t, Celsius2Fahrenheit(t))
 
 def getImageObj(url):
     return document.Image(URL_GOOGLE+url)
 
-def gooleWeatherConverterRTF(weatherData, doc):
+def gooleWeatherConverter(weatherData, doc):
     '''convert data to html/txt'''
     doc.AppendText('\n\n')
     doc.AppendElement(getImageObj(weatherData['current_conditions']['icon']))
     doc.AppendText('\nCity: %s\n' % weatherData['forecast_information']['city'])
     doc.AppendText('Current Condition: %s\n' % weatherData['current_conditions']['condition'])
     doc.AppendText('%s\n' % weatherData['current_conditions']['humidity'])
-    doc.AppendText('Temperature: %s C\n' % weatherData['current_conditions']['temp_c'])
+    doc.AppendText('Temperature: %sC(%sF)\n' % (weatherData['current_conditions']['temp_c'],
+        weatherData['current_conditions']['temp_f'])
     doc.AppendText('%s\n' % weatherData['current_conditions']['wind_condition'])
     doc.AppendText('\nForecasts:\n')
     for day in weatherData['forecasts']:
         doc.AppendElement(getImageObj(day['icon']))
-        doc.AppendText(' %s: ' % day['day_of_week'])
-        doc.AppendText('%s, %sC ~ %sC\n'%(day['condition'],
-                Fahrenheit2Celsius(day['low']),
-                Fahrenheit2Celsius(day['high'])))
+        doc.AppendText(' %s: %s, %s ~ %s\n'%(day['day_of_week'], day['condition'],
+            TempConverter(day['low'], weatherData['forecast_information']['unit_system']),
+            TempConverter(day['high'], weatherData['forecast_information']['unit_system'])))
 
 if __name__ == '__main__':
     myRobot = robot.Robot('Dr. Weather',
@@ -113,7 +111,7 @@ if __name__ == '__main__':
             version='1.0',
             profile_url='http://shiny-sky.appspot.com/assets/profile.html')
     myRobot.RegisterHandler(events.WAVELET_SELF_ADDED, OnRobotAdded)
-    myRobot.RegisterHandler(events.BLIP_SUBMITTED, OnBlipSubmitRTF)
+    myRobot.RegisterHandler(events.BLIP_SUBMITTED, OnBlipSubmit)
     #myRobot.RegisterHandler(events.WAVELET_PARTICIPANTS_CHANGED, OnParticipantsChanged)
     
     myRobot.Run(debug=True)
