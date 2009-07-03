1d6a5650bdec3748b6babe6674daf3b73c036718
shinysky1986 <shinysky1986@9a49b822-6227-11de-b259-67f869b4f23a>
2009-07-03 02:29:02 +0000

    try...catch exceptions

--- Robot/Dr.Weather/gwapi.py
+++ Robot/Dr.Weather/gwapi.py
@@ -57,29 +57,32 @@ def get_weather_from_google(location_id, hl = ''):
     dom = minidom.parseString(xml_response)    
     handler.close()
 
-    weather_data = {}
-    weather_dom = dom.getElementsByTagName('weather')[0]
+    try:
+        weather_data = {}
+        weather_dom = dom.getElementsByTagName('weather')[0]
 
-    data_structure = {
-        'forecast_information': ('city', 'postal_code', 'latitude_e6', 'longitude_e6', 'forecast_date', 'current_date_time', 'unit_system'),
-        'current_conditions': ('condition','temp_f', 'temp_c', 'humidity', 'wind_condition', 'icon')
-    }          
-    for (tag, list_of_tags2) in data_structure.iteritems():
-        tmp_conditions = {}
-        for tag2 in list_of_tags2:
-            tmp_conditions[tag2] =  weather_dom.getElementsByTagName(tag)[0].getElementsByTagName(tag2)[0].getAttribute('data')
-        weather_data[tag] = tmp_conditions
+        data_structure = {
+            'forecast_information': ('city', 'postal_code', 'latitude_e6', 'longitude_e6', 'forecast_date', 'current_date_time', 'unit_system'),
+            'current_conditions': ('condition','temp_f', 'temp_c', 'humidity', 'wind_condition', 'icon')
+        }          
+        for (tag, list_of_tags2) in data_structure.iteritems():
+            tmp_conditions = {}
+            for tag2 in list_of_tags2:
+                tmp_conditions[tag2] =  weather_dom.getElementsByTagName(tag)[0].getElementsByTagName(tag2)[0].getAttribute('data')
+            weather_data[tag] = tmp_conditions
 
-    forecast_conditions = ('day_of_week', 'low', 'high', 'icon', 'condition')
-    forecasts = []
-   
-    for forecast in dom.getElementsByTagName('forecast_conditions'):
-        tmp_forecast = {}
-        for tag in forecast_conditions:
-            tmp_forecast[tag] = forecast.getElementsByTagName(tag)[0].getAttribute('data')
-        forecasts.append(tmp_forecast)
+        forecast_conditions = ('day_of_week', 'low', 'high', 'icon', 'condition')
+        forecasts = []
+       
+        for forecast in dom.getElementsByTagName('forecast_conditions'):
+            tmp_forecast = {}
+            for tag in forecast_conditions:
+                tmp_forecast[tag] = forecast.getElementsByTagName(tag)[0].getAttribute('data')
+            forecasts.append(tmp_forecast)
 
-    weather_data['forecasts'] = forecasts    
-    dom.unlink()
+        weather_data['forecasts'] = forecasts    
+        dom.unlink()
 
-    return weather_data
+        return weather_data
+    except:
+        return None
