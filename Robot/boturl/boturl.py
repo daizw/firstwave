86ef667a9a5ecf5e5ede467f137335eec63bc525
shinysky1986 <shinysky1986@9a49b822-6227-11de-b259-67f869b4f23a>
2009-07-25 10:46:09 +0000

    added support for bit.ly

--- Robot/boturl/boturl.py
+++ Robot/boturl/boturl.py
@@ -20,9 +20,12 @@ from waveapi import events
 from waveapi import model
 from waveapi import robot
 from waveapi import document
+from waveapi import simplejson
 
 URL_TINYURL = 'http://tinyurl.com/api-create.php?url=%s'
 URL_TINYURL_PREVIEW = 'http://tinyurl.com/preview.php?num=%s'
+
+URL_BITLY_EXPAND = 'http://api.bit.ly/expand?version=2.0.1&shortUrl=%s&login=bitlyapidemo&apiKey=R_0da49e0a9118ff35f52f629d2d71bf07'
 
 STR_USAGE = "Usage:\nJust add me as a participant, I'll take care of the rest\n"
 STR_LINK_TEXT = '[%s/...]'
@@ -40,6 +43,13 @@ def OnRobotAdded(properties, context):
 def getMatchGroup(text):
     """return URL match groups"""
     return re.findall(r"(?i)((https?|ftp|telnet|file|ms-help|nntp|wais|gopher|notes|prospero):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&]*))", text)
+
+def httpGet(url):
+    logger.debug('urlfetch: %s' % url)
+    handler = urllib2.urlopen(url)
+    response = handler.read()
+    handler.close()
+    return response
 
 def OnBlipSubmit(properties, context):
     """Invoked when new blip submitted."""
@@ -62,22 +72,34 @@ def OnBlipSubmit(properties, context):
             continue
         replaceRange = document.Range(left, left+len(q[0]))
         linkTxt = None
-        tMat = re.match('^http://tinyurl.com/(\w+)$', q[0])
-        if tMat:
-            #http://tinyurl.com/preview.php?num=dehdc
-            url = URL_TINYURL_PREVIEW % (tMat.groups()[0])
-            logger.debug('urlfetch: %s' % url)
-            handler = urllib2.urlopen(url)
-            response = handler.read()
-            handler.close()
-            oriurls = re.findall(r'(?i)<a id="redirecturl" href="([^<>]+)">', response)
-            if oriurls:
-                oMat = getMatchGroup(oriurls[0])
-                if oMat:
-                    q = oMat[0]
-                else:#it also possibly can't be matched by our regex
-                    q = (oriurls[0],)
-                    linkTxt = STR_LINK_TEXT_S % oriurls[0]
+        if q[0].startswith('http://tinyurl.com/'):
+            tMat = re.match('^http://tinyurl.com/(\w+)$', q[0])
+            if tMat:
+                #http://tinyurl.com/preview.php?num=dehdc
+                url = URL_TINYURL_PREVIEW % (tMat.groups()[0])
+                response = httpGet(url)
+                oriurls = re.findall(r'(?i)<a id="redirecturl" href="([^<>]+)">', response)
+                if oriurls:
+                    oMat = getMatchGroup(oriurls[0])
+                    if oMat:
+                        q = oMat[0]
+                    else:#it also possibly can't be matched by our regex
+                        q = (oriurls[0],)
+                        linkTxt = STR_LINK_TEXT_S % oriurls[0]
+        elif q[0].startswith('http://bit.ly/'):
+            bMat = re.match('^http://bit.ly/(\w+)$', q[0])
+            if bMat:
+                url = URL_BITLY_EXPAND % q[0]
+                bJson = simplejson.loads(httpGet(url))
+                #{u'errorCode': 0, u'errorMessage': u'', u'results': {u'31IqMl': {u'longUrl': u'http://cnn.com/'}}, u'statusCode': u'OK'}
+                if bJson[u'errorCode'] == 0:
+                    oriurl = bJson[u'results'][bMat.groups()[0]][u'longUrl']
+                    oMat = getMatchGroup(oriurl)
+                    if oMat:
+                        q = oMat[0]
+                    else:
+                        q = (oriurl,)
+                        linkTxt = STR_LINK_TEXT_S % oriurl
 
         if linkTxt == None:
             domain = q[5].replace('\\','/')
